6d1f88c2c7852fcda3feb36e6fcecaf72ee242c6
Sager Williams <sagerwilliams0011@gmail.com>
2019-10-01 16:37:51 -0500

    Added all dependencies to liri.js

--- liri.js
+++ liri.js
@@ -1,2 +1,12 @@
+// node-spotify-api
+var Spotify = require('node-spotify-api');
+// axios
+const axios = require('axios');
+// moment
+var moment = require('moment');
+// dotenv
 require("dotenv").config();
 var keys = require("./keys.js");
+
+// spotify keys
+var spotifyKey = new Spotify(keys.spotify);
